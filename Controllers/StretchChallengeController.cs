4a7e4ee93e3724725fb4f140a2ddda15bba11e4c
Anton Staykov <astaykov@gmail.com>
2020-01-07 17:32:41 +0100

    b2c web api core

---
+++ Controllers/StretchChallengeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using b2c_webapi_core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace b2c_webapi_core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class StretchChallengeController : ControllerBase
+    {
+
+        private readonly ILogger<StretchChallengeController> _logger;
+
+        public StretchChallengeController(ILogger<StretchChallengeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async IAsyncEnumerable<ChallengeResultModel> Get()
+        {
+            _logger.LogInformation("Calling ChallengeResult");
+            foreach (Claim c in User.Claims)
+            {
+                yield return new ChallengeResultModel {
+                    ClaimType = c.Type,
+                    ClaimValue = c.Value
+                };
+            }
+        }
+    }
+}
---
+++ Models/ChallengeResultModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace b2c_webapi_core.Models
+{
+    public class ChallengeResultModel
+    {
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+    }
+}
